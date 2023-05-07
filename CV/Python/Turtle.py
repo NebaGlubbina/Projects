4590e06868bb2a7b351fd518174ad9d355db34ad
NebaGlubbina <132818662+NebaGlubbina@users.noreply.github.com>
2023-05-07 13:46:50 +0300

    Add files via upload

---
+++ CV/Python/Turtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color('red')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+import turtle
+
+turtle.shape("turtle")
+turtle.color('green')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
